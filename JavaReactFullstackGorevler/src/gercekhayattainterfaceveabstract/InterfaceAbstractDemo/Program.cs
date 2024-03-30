f2ae0abcf7bcfad82f7dc26949f9f499de5255ef
RobinTR <memrobincen@gmail.com>
2024-03-30 16:09:27 +0300

    Asenkron Gorevler: Gerçek Hayatta Interface ve Abstract

---
+++ JavaReactFullstackGorevler/src/gercekhayattainterfaceveabstract/InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceAbstractDemo.Abstracts;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concretes;
+using InterfaceAbstractDemo.Entities;
+
+namespace InterfaceAbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth = new DateTime(1985,1,6), FirstName = "Engin", LastName = "Demiroğ", NationalityId = "11111111111"});
+
+            Console.ReadLine();
+        }
+    }
+}
